49de430e65e590ab774d7490aba0bab732719cfc
MichalGuz <michalguz@gmail.com>
2020-01-04 22:40:43 +0100

    Create getTasks and getTask methods with simple implementions in class TaskController.

--- src/main/java/com/crud/kanban_app/controller/TaskController.java
+++ src/main/java/com/crud/kanban_app/controller/TaskController.java
@@ -1,4 +1,16 @@
 package com.crud.kanban_app.controller;
 
+import com.crud.kanban_app.domain.TaskDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskController {
+    public List<TaskDto> getTasks() {
+        return new ArrayList<>();
+    }
+
+    public TaskDto getTask(long id){
+        return new TaskDto(1L, "Test title", "Test content");
+    }
 }
