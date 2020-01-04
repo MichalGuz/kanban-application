de6cdb649f07e339915bbcee69b2fb884699514a
MichalGuz <michalguz@gmail.com>
2020-01-04 22:33:22 +0100

    After successfull printinge values of TaskDto instance in console remove from class KanbanApplication code with creation of TaskDto object.

--- src/main/java/com/crud/kanban_app/KanbanApplication.java
+++ src/main/java/com/crud/kanban_app/KanbanApplication.java
@@ -1,6 +1,5 @@
 package com.crud.kanban_app;
 
-import com.crud.kanban_app.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,17 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KanbanApplication {
 
 	public static void main(String[] args) {
-		TaskDto taskDto = new TaskDto(
-				(long)1,
-				"Test of Lombok",
-				"First use of Lombok");
-
-		Long id = taskDto.getId();
-		String  title = taskDto.getTitle();
-		String content = taskDto.getContent();
-
-		System.out.println(id + " " + title + " " + content);
-
 		SpringApplication.run(KanbanApplication.class, args);
 	}
 
