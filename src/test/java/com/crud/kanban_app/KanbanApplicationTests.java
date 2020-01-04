929e644d4ac59ab4e864d12a0e141fafd598c707
MichalGuz <michalguz@gmail.com>
2020-01-04 21:29:14 +0100

    Add files to repository kanban application.

---
+++ src/test/java/com/crud/kanban_app/KanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.kanban_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
