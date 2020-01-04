88970676b490eb331c82b96caea0a5a1dfb9f51c
MichalGuz <michalguz@gmail.com>
2020-01-04 22:07:27 +0100

    Try to create new object of class TaskDto in KanbanApplication class and check operation of Lombok annotations.

--- src/main/java/com/crud/kanban_app/KanbanApplication.java
+++ src/main/java/com/crud/kanban_app/KanbanApplication.java
@@ -1,5 +1,6 @@
 package com.crud.kanban_app;
 
+import com.crud.kanban_app.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KanbanApplication {
 
 	public static void main(String[] args) {
+		TaskDto taskDto = new TaskDto(
+				(long)1,
+				"Test of Lombok",
+				"First use of Lombok");
 		SpringApplication.run(KanbanApplication.class, args);
 	}
 
