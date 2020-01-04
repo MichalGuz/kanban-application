e18d664b5497bb5650d99ad9fc2874bc024a7deb
MichalGuz <michalguz@gmail.com>
2020-01-04 22:28:09 +0100

    Try to retrieve values of TaskDto instance using getters created by Lombok annotation.

--- src/main/java/com/crud/kanban_app/KanbanApplication.java
+++ src/main/java/com/crud/kanban_app/KanbanApplication.java
@@ -12,6 +12,11 @@ public class KanbanApplication {
 				(long)1,
 				"Test of Lombok",
 				"First use of Lombok");
+
+		Long id = taskDto.getId();
+		String  title = taskDto.getTitle();
+		String content = taskDto.getContent();
+
 		SpringApplication.run(KanbanApplication.class, args);
 	}
 
