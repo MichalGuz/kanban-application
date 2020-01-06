eaf7f3941cbe408fef57335798d120227414cd32
MichalGuz <michalguz@gmail.com>
2020-01-06 11:39:35 +0100

    Delete unusefull import from TaskController class.

--- src/main/java/com/crud/kanban_app/controller/TaskController.java
+++ src/main/java/com/crud/kanban_app/controller/TaskController.java
@@ -1,7 +1,6 @@
 package com.crud.kanban_app.controller;
 
 import com.crud.kanban_app.domain.TaskDto;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
