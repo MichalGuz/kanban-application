8c22ece89842af9068d10f3fba1b3a1e68b1ba1a
MichalGuz <michalguz@gmail.com>
2020-01-12 23:27:17 +0100

    Remove annotation @Repository to TaskRepository interface and import linked to this annotation.

--- src/main/java/com/crud/kanban_app/TaskRepository.java
+++ src/main/java/com/crud/kanban_app/TaskRepository.java
@@ -2,8 +2,6 @@ package com.crud.kanban_app;
 
 import com.crud.kanban_app.domain.Task;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
 }
