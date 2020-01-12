89dd382f44cf42705c0643b5a6f97a26124a9533
MichalGuz <michalguz@gmail.com>
2020-01-12 23:28:49 +0100

    Override method findAll() in  TaskRepository interface.

--- src/main/java/com/crud/kanban_app/TaskRepository.java
+++ src/main/java/com/crud/kanban_app/TaskRepository.java
@@ -3,5 +3,9 @@ package com.crud.kanban_app;
 import com.crud.kanban_app.domain.Task;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends CrudRepository<Task, Long> {
+    @Override
+    List<Task> findAll();
 }
