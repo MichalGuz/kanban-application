4d6b2957fd4f2b711cec58c0aa32485faef4e55e
MichalGuz <michalguz@gmail.com>
2020-01-12 23:44:58 +0100

    Create TaskRepository field of class DbService with annotation @Autowired.

--- src/main/java/com/crud/kanban_app/service/DbService.java
+++ src/main/java/com/crud/kanban_app/service/DbService.java
@@ -1,7 +1,12 @@
 package com.crud.kanban_app.service;
 
+import com.crud.kanban_app.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DbService {
+    @Autowired
+    private TaskRepository taskRepository;
+
 }
