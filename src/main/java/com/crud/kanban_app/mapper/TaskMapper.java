4c21163292305efc731f3ae9c9b87988e8a45067
MichalGuz <michalguz@gmail.com>
2020-01-13 00:00:21 +0100

    Complete implementation of  mapToTask() method in TaskMapper class.

--- src/main/java/com/crud/kanban_app/mapper/TaskMapper.java
+++ src/main/java/com/crud/kanban_app/mapper/TaskMapper.java
@@ -4,6 +4,10 @@ import com.crud.kanban_app.domain.TaskDto;
 
 public class TaskMapper {
     public class Task mapToTask(final TaskDto taskDto) {
-
+        return new Task(
+                taskDto.getId(),
+                taskDto.getTitle(),
+                taskDto.getContent()
+        );
     }
 }
