292035129a8dc9e2a43aa086cd203d526460f2d2
MichalGuz <michalguz@gmail.com>
2020-01-12 23:21:27 +0100

    Interface TaskRepository extends CrudRepository<Task, Long>.

--- src/main/java/com/crud/kanban_app/TaskRepository.java
+++ src/main/java/com/crud/kanban_app/TaskRepository.java
@@ -1,4 +1,7 @@
 package com.crud.kanban_app;
 
-public interface TaskRepository {
+import com.crud.kanban_app.domain.Task;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TaskRepository extends CrudRepository<Task, Long> {
 }
