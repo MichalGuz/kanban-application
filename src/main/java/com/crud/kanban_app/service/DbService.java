76725ee87aadd6f2e8aee236bd1b967c78b0117d
MichalGuz <michalguz@gmail.com>
2020-01-12 23:47:46 +0100

    Create getAllTasks() method in DbService class.

--- src/main/java/com/crud/kanban_app/service/DbService.java
+++ src/main/java/com/crud/kanban_app/service/DbService.java
@@ -1,12 +1,18 @@
 package com.crud.kanban_app.service;
 
+import com.crud.kanban_app.domain.Task;
 import com.crud.kanban_app.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DbService {
     @Autowired
     private TaskRepository taskRepository;
 
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
 }
