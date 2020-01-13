b9c79cecc6f34aa78a2998c86cdc0b56d5aed472
MichalGuz <michalguz@gmail.com>
2020-01-13 23:01:37 +0100

    Create mapToTaskDtoList() method in TaskMapper class.

--- src/main/java/com/crud/kanban_app/mapper/TaskMapper.java
+++ src/main/java/com/crud/kanban_app/mapper/TaskMapper.java
@@ -2,6 +2,10 @@ package com.crud.kanban_app.mapper;
 
 import com.crud.kanban_app.domain.Task;
 import com.crud.kanban_app.domain.TaskDto;
+import sun.text.resources.sk.JavaTimeSupplementary_sk;
+
+import java.util.List;
+
 
 public class TaskMapper {
     public Task mapToTask(final TaskDto taskDto) {
@@ -17,4 +21,6 @@ public class TaskMapper {
                 task.getTitle(),
                 task.getContent());
     }
+
+    public List<TaskDto> mapToTaskDtoList(final List<Task> taskList) {}
 }
