50fe6db1f589616453c5ebc74fdbcfcfa2259b40
MichalGuz <michalguz@gmail.com>
2020-01-27 23:11:51 +0100

    Class KanbanApplication extends SpringBootServletInitializer .

--- src/main/java/com/crud/kanban_app/KanbanApplication.java
+++ src/main/java/com/crud/kanban_app/KanbanApplication.java
@@ -2,12 +2,13 @@ package com.crud.kanban_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
 
-public class KanbanApplication {
+public class KanbanApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KanbanApplication.class, args);
